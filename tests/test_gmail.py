7d57b74a03d228775921d5a1bf77b49d38075cbd
David V. Lu <davidvlu@gmail.com>
2024-04-11 16:10:04 -0400

    GMail API

---
+++ tests/test_gmail.py
@@ -0,0 +1,22 @@
+from googly import GMailAPI
+from secret_auth import get_secrets
+
+
+def test_basic_access():
+    api = GMailAPI(**get_secrets())
+
+    threads = api.get_threads('in:anywhere')
+
+    assert threads
+
+    for thread_id, thread in threads.items():
+        if thread_id != '18ec87a5013ae2d9':
+            continue
+        assert len(thread) == 2
+
+        msg = api.get_message(thread[-1]['id'])
+        assert msg['snippet'].startswith('Lorem ipsum')
+
+        headers = {a['name']: a['value'] for a in msg['payload']['headers']}
+        assert headers['Subject'] == 'This is a test email'
+        assert 'davidvlu' in headers['From']

3c6a906cce65ed354e742de6a8e4851e98d1449a
David V. Lu <davidvlu@gmail.com>
2024-04-10 22:20:46 -0400

    Calendar API

--- src/googly/__init__.py
+++ src/googly/__init__.py
@@ -1,5 +1,6 @@
 from .api import API
+from .apps.calendar import CalendarAPI
 from .apps.drive import DriveAPI
 from .apps.sheets import SheetsAPI
 
-__all__ = ['API', 'DriveAPI', 'SheetsAPI']
+__all__ = ['API', 'CalendarAPI', 'DriveAPI', 'SheetsAPI']
--- src/googly/api.py
+++ src/googly/api.py
@@ -49,9 +49,11 @@ class API:
 
         self.service = build(name, version, credentials=creds, **kwargs)
 
-    def get_paged_result(self, api_method, result_keyword, **kwargs):
+    def get_paged_result(self, api_method, result_keyword, max_results=0, max_results_param_name='pageSize', **kwargs):
         next_token = None
         seen = 0
+        if max_results:
+            kwargs[max_results_param_name] = max_results
 
         while True:
             results = api_method(
@@ -63,5 +65,5 @@ class API:
             seen += len(results[result_keyword])
             next_token = results.get('nextPageToken')
 
-            if not next_token:
+            if not next_token or (max_results and seen >= max_results):
                 break

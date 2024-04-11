031de2cfff385b2dd9d030c51d8438ca66ef4d9a
David V. Lu!! <davidvlu@gmail.com>
2024-04-11 16:21:22 -0400

    GMail API (#6)
    * GMail API
    
    * Pass environment variable

--- src/googly/__init__.py
+++ src/googly/__init__.py
@@ -1,6 +1,7 @@
 from .api import API
 from .apps.calendar import CalendarAPI
 from .apps.drive import DriveAPI
+from .apps.gmail import GMailAPI
 from .apps.sheets import SheetsAPI
 
-__all__ = ['API', 'CalendarAPI', 'DriveAPI', 'SheetsAPI']
+__all__ = ['API', 'CalendarAPI', 'DriveAPI', 'GMailAPI', 'SheetsAPI']
---
+++ src/googly/apps/gmail.py
@@ -0,0 +1,32 @@
+import googly
+import collections
+
+
+class GMailAPI(googly.API):
+    # https://developers.google.com/gmail/api
+
+    def __init__(self,
+                 scopes=['https://www.googleapis.com/auth/gmail.readonly'],
+                 **kwargs):
+        googly.API.__init__(self, 'gmail', 'v1', scopes, **kwargs)
+
+    def get_messages(self, query='', user_id='me', **kwargs):
+        yield from self.get_paged_result(
+            self.service.users().messages().list,
+            'messages',
+            userId=user_id,
+            q=query,
+            **kwargs
+        )
+
+    def get_threads(self, query='in:inbox', user_id='me'):
+        threads = collections.defaultdict(list)
+        for m in self.get_messages(query, user_id):
+            threads[m['threadId']].append(m)
+        return dict(threads)
+
+    def get_message(self, msg_id, user_id='me'):
+        return self.service.users().messages().get(
+            userId=user_id,
+            id=msg_id
+        ).execute()

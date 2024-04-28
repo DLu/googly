f9ed616a21a29f271cedf7518b6474e513aecedd
David V. Lu!! <davidvlu@gmail.com>
2024-04-27 21:34:11 -0400

    People API (#18)

--- src/googly/__init__.py
+++ src/googly/__init__.py
@@ -3,8 +3,9 @@ from .scope import Scope
 from .apps.calendar import CalendarAPI
 from .apps.drive import DriveAPI
 from .apps.gmail import GMailAPI
+from .apps.people import PeopleAPI
 from .apps.photos import PhotosAPI
 from .apps.sheets import SheetsAPI
 from .apps.youtube import YouTubeAPI
 
-__all__ = ['API', 'Scope', 'CalendarAPI', 'DriveAPI', 'GMailAPI', 'PhotosAPI', 'SheetsAPI', 'YouTubeAPI']
+__all__ = ['API', 'Scope', 'CalendarAPI', 'DriveAPI', 'GMailAPI', 'PeopleAPI', 'PhotosAPI', 'SheetsAPI', 'YouTubeAPI']

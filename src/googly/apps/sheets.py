2533cb72a65b339d9c732ea3b40f47be048f8ffb
David V. Lu <davidvlu@gmail.com>
2024-09-14 14:53:59 -0400

    Writing/Editing sheets

--- src/googly/apps/sheets.py
+++ src/googly/apps/sheets.py
@@ -3,6 +3,7 @@ import googleapiclient
 import re
 
 CELL_PATTERN = re.compile(r'^([A-Z]+)(\d+)$')
+RANGE_PATTERN = re.compile(r'^(.*)!(.*):(.*)$')
 A_I = ord('A')
 
 # Credit to
@@ -41,8 +42,9 @@ class SheetsAPI(googly.API):
 
     class Scope(googly.Scope):
         SPREADSHEETS_READONLY = 1
+        SPREADSHEETS = 2  # Read and write
 
-    def __init__(self, spreadsheetId=None, scopes=Scope.all(), **kwargs):
+    def __init__(self, spreadsheetId=None, scopes=Scope.SPREADSHEETS, **kwargs):
         googly.API.__init__(self, 'sheets', 'v4', scopes, **kwargs)
         self.api = self.service.spreadsheets()
         self.set_sheet_id(spreadsheetId)
@@ -56,6 +58,14 @@ class SheetsAPI(googly.API):
             self.metadata[self.sheet_id] = self.api.get(spreadsheetId=self.sheet_id).execute()
         return self.metadata[self.sheet_id]
 
+    def get_sheet_names(self, meta=None):
+        names = []
+        if not meta:
+            meta = self.get_metadata()
+        for sheet in meta['sheets']:
+            names.append(sheet['properties']['title'])
+        return names
+
     def get_sheet_info(self, sheet):
         if sheet is None:
             sheet = 0
@@ -67,10 +77,17 @@ class SheetsAPI(googly.API):
             if len(matching) == 1:
                 return matching[0]
             else:
-                names = [info['properties']['title'] for info in meta['sheets']]
+                names = self.get_sheet_names(meta)
                 raise IndexError(f'Unable to find sheet named {sheet}. Valid names are {names}')
         else:
             raise IndexError(f'Cannot get sheet {sheet}')
+
+    def get_sheet_name(self, sheet):
+        sheet_info = self.get_sheet_info(sheet)
+        return sheet_info['properties']['title']
+
+    def has_sheet_name(self, sheet_name):
+        return sheet_name in self.get_sheet_names()
 
     def get_size(self, sheet=None):
         sheet_info = self.get_sheet_info(sheet)
@@ -91,10 +108,27 @@ class SheetsAPI(googly.API):
         if isinstance(cell, tuple):
             cell = coord_to_cell(*cell)
 
-        return self.get_values(cell)['values'][0][0]
+        value_matrix = self.get_values(cell)
+        if 'values' not in value_matrix:
+            return ''
+        else:
+            return value_matrix['values'][0][0]
 
     def get_range(self, range_spec=None):
-        return self.get_values(range_spec)['values']
+        ret = self.get_values(range_spec)
+        if 'values' in ret:
+            return ret['values']
+        else:
+            m = RANGE_PATTERN.match(ret['range'])
+            start = cell_to_coord(m.group(2))
+            end = cell_to_coord(m.group(3))
+
+            width = end[0] - start[0] + 1
+            height = end[1] - start[1] + 1
+            values = []
+            for h in range(height):
+                values.append([''] * width)
+            return values
 
     def get_contents(self, sheet=None):
         sheet_info = self.get_sheet_info(sheet)
@@ -116,3 +150,71 @@ class SheetsAPI(googly.API):
                 continue
             rows.append(dict(zip(labels, row)))
         return rows
+
+    def set_contents(self, values, start_cell='A1', row_major=True, raw=False, sheet=None):
+        sheet_name = self.get_sheet_name(sheet)
+        c0, r0 = cell_to_coord(start_cell)
+        if row_major:
+            r1 = r0 + len(values) - 1
+            c1 = c0 + len(values[0]) - 1
+        else:
+            c1 = c0 + len(values) - 1
+            r1 = r0 + len(values[0]) - 1
+        end_cell = coord_to_cell(c1, r1)
+        range_name = f'{sheet_name}!{start_cell}:{end_cell}'
+
+        return self.api.values().update(
+            spreadsheetId=self.sheet_id,
+            range=range_name,
+            valueInputOption='RAW' if raw else 'USER_ENTERED',
+            body={
+                'range': range_name,
+                'majorDimension': 'ROWS' if row_major else 'COLUMNS',
+                'values': values,
+            }
+        ).execute()
+
+    def clear_contents(self, ranges=None, sheet=None):
+        sheet_name = self.get_sheet_name(sheet)
+        full_ranges = []
+        if ranges:
+            for range_spec in ranges:
+                if '!' in range_spec:
+                    full_ranges.append(range_spec)
+                else:
+                    full_ranges.append(f'{sheet_name}!{range_spec}')
+        else:
+            col, row = self.get_size(sheet)
+            end_cell = coord_to_cell(col, row)
+            full_ranges.append(f'{sheet_name}!A1:{end_cell}')
+        return self.api.values().batchClear(
+            spreadsheetId=self.sheet_id,
+            body={
+                'ranges': full_ranges,
+            }
+        ).execute()
+
+    def batch_update(self, requests):
+        if not isinstance(requests, list):
+            requests = [requests]
+
+        return self.api.batchUpdate(
+            spreadsheetId=self.sheet_id,
+            body={
+                'requests': requests
+            }
+        ).execute()
+
+    def add_sheet(self, title=None):
+        if self.has_sheet_name(title):
+            return
+
+        self.metadata = {}
+        return self.batch_update({'addSheet': {'properties': {'title': title}}})
+
+    def delete_sheet(self, sheet=None):
+        sheet_info = self.get_sheet_info(sheet)
+        sheet_id = sheet_info['properties']['sheetId']
+
+        self.metadata = {}
+        return self.batch_update({'deleteSheet': {'sheetId': sheet_id}})
--- tests/test_sheets.py
+++ tests/test_sheets.py
@@ -62,6 +62,13 @@ def test_basic_access():
     assert api.get_size(1) == (26, 1000)
     assert api.get_size('Pitching') == (26, 1000)
 
+    assert api.has_sheet_name('Batting')
+    assert api.has_sheet_name('Pitching')
+    assert not api.has_sheet_name('Fielding')
+
+    assert api.get_sheet_name(0) == 'Batting'
+    assert api.get_sheet_name(1) == 'Pitching'
+
     with pytest.raises(IndexError):
         api.get_size(2)
 
@@ -80,6 +87,80 @@ def test_basic_access():
 
 
 def test_no_sheet_id():
-    api = SheetsAPI(scopes=SheetsAPI.Scope.SPREADSHEETS_READONLY, **get_credentials())
+    api = SheetsAPI(**get_credentials())
     with pytest.raises(Exception):
         api.get_value('A1')
+
+
+def test_writing():
+    api = SheetsAPI(**get_credentials())
+    api.set_sheet_id('1QegYhRutctw5pogpJMFglHsEBTMYlUg8XGE4dvIz9iE')
+
+    try:
+
+        assert api.has_sheet_name('Sheet1')
+        assert not api.has_sheet_name('Sheet2')
+
+        api.clear_contents()
+
+        alphabet_data = [
+            ['A', 'B', 'C', 'D'],
+            ['E', 'F', 'G', 'H'],
+            ['I', 'J', 'K', 'L'],
+            ['M', 'N', 'O', 'P'],
+        ]
+
+        api.set_contents(alphabet_data)
+        assert api.get_value('A1') == 'A'
+        assert api.get_value('B1') == 'B'
+        assert api.get_value('A2') == 'E'
+        assert api.get_value('C3') == 'K'
+        assert api.get_value('D3') == 'L'
+        assert api.get_value('D4') == 'P'
+
+        api.clear_contents(['A1:B4'])
+        assert api.get_value('A1') == ''
+        assert api.get_value('B1') == ''
+        assert api.get_value('A2') == ''
+        assert api.get_value('C3') == 'K'
+        assert api.get_value('D3') == 'L'
+        assert api.get_value('D4') == 'P'
+
+        api.set_contents(alphabet_data, row_major=False)
+        assert api.get_value('A1') == 'A'
+        assert api.get_value('B1') == 'E'
+        assert api.get_value('A2') == 'B'
+        assert api.get_value('C3') == 'K'
+        assert api.get_value('D3') == 'O'
+        assert api.get_value('D4') == 'P'
+
+        api.add_sheet('Sheet2')
+        assert api.has_sheet_name('Sheet1')
+        assert api.has_sheet_name('Sheet2')
+
+        # Get empty values
+        grid = api.get_contents('Sheet2')
+        assert len(grid) == 1000
+        for row in grid:
+            assert len(row) == 26
+            assert all(not a for a in row)
+
+        api.clear_contents(['Sheet1!A1:A4', 'Sheet1!C1:C4'])
+        assert api.get_value('A1') == ''
+        assert api.get_value('B1') == 'E'
+        assert api.get_value('A2') == ''
+        assert api.get_value('C3') == ''
+        assert api.get_value('D3') == 'O'
+        assert api.get_value('D4') == 'P'
+
+        # Avoid duplicate insertion
+        assert len(api.get_sheet_names()) == 2
+        api.add_sheet('Sheet1')  # Should do nothing
+        assert len(api.get_sheet_names()) == 2
+
+    finally:
+        api.delete_sheet('Sheet2')
+        assert api.has_sheet_name('Sheet1')
+        assert not api.has_sheet_name('Sheet2')
+
+        api.clear_contents(sheet='Sheet1')

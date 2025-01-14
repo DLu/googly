ea1ac7eb3e2f47880700df5959af23690fe40468
David V. Lu <davidvlu@gmail.com>
2025-01-14 14:34:17 -0500

    YouTube Playlist Functionality

--- tests/test_yt.py
+++ tests/test_yt.py
@@ -1,5 +1,7 @@
 from googly import YouTubeAPI
+from googly.apps.youtube import parse_isoduration
 from creds import get_credentials
+import pytest
 
 
 def test_basic_access():
@@ -21,6 +23,27 @@ def test_basic_access():
     assert channel['channelId'] == 'UCymhhrwqneZRBsgnVPtlwzQ'
 
 
+def test_parts():
+    api = YouTubeAPI(**get_credentials())
+
+    with pytest.raises(RuntimeError):
+        list(api.get_subscription_list(parts=['bogus']))
+
+    subscriptions = list(api.get_subscription_list(
+        parts=['contentDetails', 'id', 'subscriberSnippet']
+    ))
+
+    assert len(subscriptions) == 1
+
+    sub = subscriptions[0]
+    assert 'contentDetails' in sub
+    assert 'etag' in sub
+    assert 'id' in sub
+    assert 'snippet' not in sub
+    assert 'kind' in sub
+    assert 'subscriberSnippet' in sub
+
+
 def test_basic_search():
     api = YouTubeAPI(**get_credentials())
 
@@ -29,3 +52,37 @@ def test_basic_search():
 
     vids = list(api.search('iron man', max_results=14))
     assert len(vids) == 14
+
+
+def test_isoduration():
+    assert parse_isoduration('PT1H5M26S') == 3926
+    assert parse_isoduration('P4DT12H30M5S') == 390605
+    assert parse_isoduration('PT42S') == 42
+
+
+def test_playlists():
+    api = YouTubeAPI(**get_credentials())
+    playlists = list(api.get_playlists())
+
+    assert len(playlists) == 1
+
+    playlist = playlists[0]
+    assert playlist['snippet']['title'] == 'Testing Playlist'
+
+    contents = list(api.get_playlist(playlist['id']))
+    assert len(contents) == 2
+
+    assert contents[0]['snippet']['title'] == 'Giant LEGO Forklift - Mantis Hacks E9'
+
+
+def test_video_info():
+    api = YouTubeAPI(**get_credentials())
+
+    # No Parts
+    info = api.get_video_info('OPCm9aoIxUs')
+    assert info['snippet']['title'] == 'Working Lego Minifig Light Saber! - Mantis Hacks'
+    assert 'lightsaber' in info['snippet']['tags']
+
+    # No Matching Video
+    with pytest.raises(RuntimeError):
+        api.get_video_info('000')

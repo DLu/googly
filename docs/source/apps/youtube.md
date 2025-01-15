ae091063399d974c8b11c9b36f2a4f7194fc0246
David V. Lu!! <davidvlu@gmail.com>
2025-01-15 13:28:42 -0500

    YouTube Playlist Functionality (#41)

--- docs/source/apps/youtube.md
+++ docs/source/apps/youtube.md
@@ -1,6 +1,29 @@
 # YouTube
 
+## Parts
+Many of the YouTube API functions have a `part` variable to specify which information you want returned.
+
+With the `googly` YouTube API, you specify a list of strings as the `parts` parameter to the calls.
+The valid values for the parts will be linked in the documentation below.
+
+## Getting Video Information
+[Valid Parts](https://developers.google.com/youtube/v3/docs/videos/list#parameters)
+
+Note that the `duration` field of the `contentDetails` part is [encoded with the "ISO 8601" standard](https://developers.google.com/youtube/v3/docs/videos#contentDetails.duration).
+A utility function is included to decode the field (without additional dependencies).
+
+```python
+from googly import YouTubeAPI
+from googly.apps.youtube import parse_isoduration
+
+api = YouTubeAPI()
+print(api.get_video_info('r8E0CbYZcjE'))
+info = api.get_video_info('r8E0CbYZcjE', parts=['contentDetails', 'statistics'])
+print(parse_isoduration(info['contentDetails']['duration']))
+```
+
 ## Listing Subscriptions
+[Valid Parts](https://developers.google.com/youtube/v3/docs/subscriptions/list#parameters)
 
 ```python
 from googly import YouTubeAPI
@@ -13,6 +36,7 @@ for sub in api.get_subscription_list():
 This method returns [Subscription objects](https://developers.google.com/youtube/v3/docs/subscriptions#resource)
 
 ## Searching for Videos
+[Valid Parts](https://developers.google.com/youtube/v3/docs/search/list#parameters)
 ```python
 from googly import YouTubeAPI
 
@@ -20,3 +44,27 @@ api = YouTubeAPI()
 for video in api.search('leeroy jenkins'):
     print(video)
 ```
+
+## Playlists
+[Valid Parts](https://developers.google.com/youtube/v3/docs/playlists/list#parameters)
+```python
+from googly import YouTubeAPI
+
+api = YouTubeAPI()
+# By default, get all playlists for the authenticated user
+print(api.get_playlists())
+# You can also specify another user
+print(api.get_playlists(filter_args={'channelId': 'UCu13RTPnsShX1uvU7Xypwrg'}))
+```
+
+There is a separate API call to get the videos within a playlist. [Valid Parts](https://developers.google.com/youtube/v3/docs/playlistItems/list#parameters)
+
+```python
+from googly import YouTubeAPI
+
+api = YouTubeAPI()
+for video in api.get_playlist('PLTSAQ5KEjPVDnWraOtIeTLP2-NKToaG7y'):
+    print(video['snippet']['title'])
+```
+
+Annoyingly, [you cannot get information about certain playlists](https://developers.google.com/youtube/v3/revision_history#september-15-2016), namely History and Watch Later, through this API.
--- src/googly/apps/youtube.py
+++ src/googly/apps/youtube.py
@@ -1,4 +1,57 @@
 import googly
+import datetime
+
+
+VALID_PARTS = {
+    'subscriptions': ['contentDetails', 'id', 'snippet', 'subscriberSnippet'],
+    'search': ['id', 'snippet'],
+    'playlist_items': ['contentDetails', 'id', 'snippet', 'status'],
+    'playlists': ['contentDetails', 'id', 'localizations', 'player', 'snippet', 'status'],
+    'videos': ['contentDetails', 'fileDetails', 'id', 'liveStreamingDetails', 'localizations',
+               'paidProductPlacementDetails', 'player', 'processingDetails', 'recordingDetails',
+               'snippet', 'statistics', 'status', 'suggestions', 'topicDetails'],
+}
+
+
+def get_valid_parts(key, values):
+    valid = []
+    invalid = []
+
+    reference = VALID_PARTS[key]
+    for part in values:
+        if part in reference:
+            valid.append(part)
+        else:
+            invalid.append(part)
+
+    if invalid:
+        raise RuntimeError(f'Invalid value(s) for parts parameters: {"/".join(invalid)}')
+
+    return ','.join(valid)
+
+
+def parse_isoduration(s):
+    # Isoduration via https://stackoverflow.com/a/64232786/27837036
+    def get_isosplit(s, split):
+        if split in s:
+            n, s = s.split(split)
+        else:
+            n = 0
+        return n, s
+
+    # Remove prefix
+    s = s.split('P')[-1]
+
+    # Step through letter dividers
+    days, s = get_isosplit(s, 'D')
+    _, s = get_isosplit(s, 'T')
+    hours, s = get_isosplit(s, 'H')
+    minutes, s = get_isosplit(s, 'M')
+    seconds, s = get_isosplit(s, 'S')
+
+    # Convert all to seconds
+    dt = datetime.timedelta(days=int(days), hours=int(hours), minutes=int(minutes), seconds=int(seconds))
+    return int(dt.total_seconds())
 
 
 class YouTubeAPI(googly.API):
@@ -10,11 +63,11 @@ class YouTubeAPI(googly.API):
     def __init__(self, scopes=Scope.all(), **kwargs):
         googly.API.__init__(self, 'youtube', 'v3', scopes, **kwargs)
 
-    def get_subscription_list(self, max_results=50):
+    def get_subscription_list(self, parts=['snippet'], max_results=50):
         yield from self.get_paged_result(
             self.service.subscriptions().list,
             'items',
-            part='snippet',
+            part=get_valid_parts('subscriptions', parts),
             mine=True,
             maxResults=max_results,
         )
@@ -24,10 +77,35 @@ class YouTubeAPI(googly.API):
             self.service.search().list,
             'items',
             q=query,
-            part=','.join(parts),
+            part=get_valid_parts('search', parts),
             order=order,
             type=type,
             max_results=max_results,
             max_results_param_name='maxResults',
 
         )
+
+    def get_playlist(self, playlist_id, parts=['snippet']):
+        yield from self.get_paged_result(
+            self.service.playlistItems().list,
+            'items',
+            playlistId=playlist_id,
+            part=get_valid_parts('playlist_items', parts),
+        )
+
+    def get_playlists(self, parts=['snippet'], filter_args={'mine': True}):
+        yield from self.get_paged_result(
+            self.service.playlists().list,
+            'items',
+            part=get_valid_parts('playlists', parts),
+            **filter_args,
+        )
+
+    def get_video_info(self, video_id, parts=['snippet']):
+        result = self.service.videos().list(
+            id=video_id,
+            part=get_valid_parts('videos', parts),
+        ).execute()
+        if not result['items']:
+            raise RuntimeError(f'Video {video_id} not found')
+        return result['items'][0]
